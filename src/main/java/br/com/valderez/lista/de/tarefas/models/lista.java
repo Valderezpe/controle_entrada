48d719346c070fed202f4f0f61d8dce46f9fb196
valderez <valderezpe@gmail.com>
2023-12-13 23:42:04 -0300

    feat:  banco dados h2

--- src/main/java/br/com/valderez/lista/de/tarefas/models/lista.java
+++ src/main/java/br/com/valderez/lista/de/tarefas/models/lista.java
@@ -34,6 +34,84 @@ public class lista {
     public lista(){
         this.createsAt = LocalDateTime.now();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatesAt() {
+        return createsAt;
+    }
+
+    public void setCreatesAt(LocalDateTime createsAt) {
+        this.createsAt = createsAt;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDate getFineshedAt() {
+        return fineshedAt;
+    }
+
+    public void setFineshedAt(LocalDate fineshedAt) {
+        this.fineshedAt = fineshedAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        lista other = (lista) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "lista [id=" + id + ", title=" + title + "]";
+    }
     
 
     
